35ef45144f29351df0c77045012bc1c5f3864410
snipezyy <79554391+snipezyy@users.noreply.github.com>
2021-02-23 18:23:31 -0800

    Add files via upload
    code for mecanum wheels, intake, and shooter

---
+++ gamechanger/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
@@ -0,0 +1,120 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Team 9960 Revision 161027.0
+ * This program provides driver station control of the Team 9960 Mecanum Drive Prototype.
+ *
+ * This robot uses four VEX Mecanum wheels, each direct driven by Neverest 20 motors.
+ * It is designed as a linear op mode, and uses RUN_WITH_ENCODER motor operation.
+ *
+ * The gamepad1 right joystick is used for translation movement, while the left joystick x-axis controls rotation.
+ *
+ */
+
+@TeleOp(name="Mecanum Proto Manual", group="Linear Opmode") // @Autonomous(...) is the other common choice
+// @Disabled
+public class BasicOpMode_Linear extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontright = null;
+    private DcMotor backright = null;
+    private DcMotor frontleft = null;
+    private DcMotor backleft = null;
+    private DcMotor intake = null;
+    // declare motor speed variables
+    double RF; double LF; double RR; double LR;
+    // declare joystick position variables
+    double X1; double Y1; double X2; double Y2;
+    // operational constants
+    double joyScale = 1;
+    double motorMax = 1; // Limit motor power to this value for Andymark RUN_USING_ENCODER mode
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        /* Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step (using the FTC Robot Controller app on the phone).
+         */
+        backleft = hardwareMap.get(DcMotor.class, "back1");
+        backright = hardwareMap.get(DcMotor.class, "back2");
+        frontleft = hardwareMap.get(DcMotor.class, "front1");
+        frontright = hardwareMap.get(DcMotor.class, "front2");
+
+
+        // Set the drive motor direction:
+        // "Reverse" the motor that runs backwards when connected directly to the battery
+        // These polarities are for the Neverest 20 motors
+        frontleft.setDirection(DcMotor.Direction.REVERSE);
+        frontright.setDirection(DcMotor.Direction.FORWARD);
+        backleft.setDirection(DcMotor.Direction.REVERSE);
+        backright.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set the drive motor run modes:
+        // "RUN_USING_ENCODER" causes the motor to try to run at the specified fraction of full velocity
+        // Note: We were not able to make this run mode work until we switched Channel A and B encoder wiring into
+        // the motor controllers. (Neverest Channel A connects to MR Channel B input, and vice versa.)
+        frontleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+
+            // Reset speed variables
+            LF = 0; RF = 0; LR = 0; RR = 0;
+
+            // Get joystick values
+            Y1 = -gamepad1.right_stick_y * joyScale; // invert so up is positive
+            X1 = gamepad1.right_stick_x * joyScale;
+            Y2 = -gamepad1.left_stick_y * joyScale; // Y2 is not used at present
+            X2 = gamepad1.left_stick_x * joyScale;
+
+            // Forward/back movement
+            LF += Y1; RF += Y1; LR += Y1; RR += Y1;
+
+            // Side to side movement
+            LF += X1; RF -= X1; LR -= X1; RR += X1;
+
+            // Rotation movement
+            LF += X2; RF -= X2; LR += X2; RR -= X2;
+
+            // Clip motor power values to +-motorMax
+            LF = Math.max(-motorMax, Math.min(LF, motorMax));
+            RF = Math.max(-motorMax, Math.min(RF, motorMax));
+            LR = Math.max(-motorMax, Math.min(LR, motorMax));
+            RR = Math.max(-motorMax, Math.min(RR, motorMax));
+
+            // Send values to the motors
+            frontleft.setPower(LF);
+            frontright.setPower(RF);
+            backleft.setPower(LR);
+            backright.setPower(RR);
+
+            // Send some useful parameters to the driver station
+            telemetry.addData("LF", "%.3f", LF);
+            telemetry.addData("RF", "%.3f", RF);
+            telemetry.addData("LR", "%.3f", LR);
+            telemetry.addData("RR", "%.3f", RR);
+        }
+    }
+}
